b05f2252b26f60a4c609454ebec8fb456b429b95
vzla0094 <vzla0094@gmail.com>
2022-05-23 09:01:28 -0700

    initial firebase setup

---
+++ firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAhWL-VE6px-42zW-veEUddTpIstjtxzJM',
+  authDomain: 'taisho-25387.firebaseapp.com',
+  projectId: 'taisho-25387',
+  storageBucket: 'taisho-25387.appspot.com',
+  messagingSenderId: '434187994962',
+  appId: '1:434187994962:web:bbd5f2e29584bba7279c8b',
+  measurementId: 'G-HM2D33LP17',
+}
+
+const app = initializeApp(firebaseConfig)
+const db = getFirestore(app)
