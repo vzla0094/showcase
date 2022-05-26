f330aa29801caab449744431e83108afe05866ee
vzla0094 <vzla0094@gmail.com>
2022-05-26 12:40:47 -0700

    Add content to SigninLoginScreen.

--- src/screens/SigninLoginScreen.tsx
+++ src/screens/SigninLoginScreen.tsx
@@ -1,9 +1,34 @@
-import { Center, Container, Text } from 'native-base'
+import {
+  Button,
+  Center,
+  Container,
+  FormControl,
+  Heading,
+  Input,
+  Stack,
+} from 'native-base'
 
 export const SigninLoginScreen = () => (
   <Center flex={1}>
-    <Container safeArea w={'100%'} flex={1}>
-      <Text>SigninLoginScreen</Text>
+    <Container
+      centerContent
+      safeArea
+      w={'100%'}
+      flex={1}
+      justifyContent="center"
+    >
+      <Heading mb={12}>Sign In to Taisho</Heading>
+      <Stack w="100%" mb={12}>
+        <FormControl>
+          <FormControl.Label>email</FormControl.Label>
+          <Input type="text"></Input>
+        </FormControl>
+        <FormControl>
+          <FormControl.Label>password</FormControl.Label>
+          <Input type="password"></Input>
+        </FormControl>
+      </Stack>
+      <Button>Sign In</Button>
     </Container>
   </Center>
 )
