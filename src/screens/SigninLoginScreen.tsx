640523dbc2d30be2da55888e2c03f9f08b642d46
vzla0094 <vzla0094@gmail.com>
2022-05-26 17:17:18 -0700

    add email validation

--- src/screens/SigninLoginScreen.tsx
+++ src/screens/SigninLoginScreen.tsx
@@ -7,28 +7,63 @@ import {
   Input,
   Stack,
 } from 'native-base'
+import { useValidations } from '../helpers/useValidations'
+import { useState } from 'react'
 
-export const SigninLoginScreen = () => (
-  <Center flex={1}>
-    <Container
-      centerContent
-      safeArea
-      w={'100%'}
-      flex={1}
-      justifyContent="center"
-    >
-      <Heading mb={12}>Sign In to Taisho</Heading>
-      <Stack w="100%" mb={12}>
-        <FormControl>
-          <FormControl.Label>email</FormControl.Label>
-          <Input type="text"></Input>
-        </FormControl>
-        <FormControl>
-          <FormControl.Label>password</FormControl.Label>
-          <Input type="password"></Input>
-        </FormControl>
-      </Stack>
-      <Button>Sign In</Button>
-    </Container>
-  </Center>
-)
+export const SigninLoginScreen = () => {
+  const [errors, validate, cleanErrors] = useValidations()
+  const [email, setEmail] = useState('')
+
+  const handleEmailBlur = (email: string) => {
+    // TODO: Add proper email validation
+    validate({
+      requireEmail: createValidation => {
+        createValidation({
+          validator: () => !!email,
+          errorMessage: 'This is a required field',
+          showToast: false,
+        })
+      },
+    })
+  }
+
+  const handleEmailChange = (text: string) => {
+    setEmail(text)
+    cleanErrors()
+  }
+
+  return (
+    <Center flex={1}>
+      <Container
+        centerContent
+        safeArea
+        w={'100%'}
+        flex={1}
+        justifyContent="center"
+      >
+        <Heading mb={12}>Register to Taisho</Heading>
+        <Stack w="100%" mb={12} space={5}>
+          <FormControl isInvalid={'requireEmail' in errors}>
+            <FormControl.Label>Email</FormControl.Label>
+            <Input
+              type="text"
+              value={email}
+              onChangeText={handleEmailChange}
+              onBlur={() => handleEmailBlur(email)}
+            ></Input>
+            {'requireEmail' in errors ? (
+              <FormControl.ErrorMessage>
+                {errors.requireEmail}
+              </FormControl.ErrorMessage>
+            ) : null}
+          </FormControl>
+          <FormControl>
+            <FormControl.Label>Password</FormControl.Label>
+            <Input type="password"></Input>
+          </FormControl>
+        </Stack>
+        <Button>Register</Button>
+      </Container>
+    </Center>
+  )
+}
