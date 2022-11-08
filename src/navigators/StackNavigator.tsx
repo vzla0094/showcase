b2bd71eabffd3d4d4d805fe699e71df223b9a8cc
vzla0094 <vzla0094@gmail.com>
2022-11-08 11:30:02 -0700

    fix LoginBottomNavigation cls

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,7 +1,6 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { RootStackParamList } from '../types'
 import { DiscoveryScreen } from '../screens/DiscoveryScreen'
-import { LoginBottomNavigation } from '../components/LoginBottomNavigation'
 import { LoginOrRegisterScreen } from '../screens/LoginOrRegisterScreen'
 
 export const StackNavigator = () => {
@@ -10,14 +9,7 @@ export const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Discovery">
-        {({ navigation }) => {
-          return (
-            <>
-              <DiscoveryScreen />
-              <LoginBottomNavigation navigation={navigation} />
-            </>
-          )
-        }}
+        {props => <DiscoveryScreen {...props} loginBottom={true} />}
       </Stack.Screen>
       <Stack.Screen name="LoginOrRegister" component={LoginOrRegisterScreen} />
     </Stack.Navigator>
--- src/screens/DiscoveryScreen.tsx
+++ src/screens/DiscoveryScreen.tsx
@@ -1,9 +1,21 @@
+import { FC } from 'react'
+import { Text } from 'native-base'
+
 import { useAppSelector } from '../hooks'
 import { DiscoveryView } from '../views/DiscoveryView'
 import { useGetActiveDealsQuery } from '../redux/services/deals'
-import { Text } from 'native-base'
+import { LoginBottomNavigation } from '../components/LoginBottomNavigation'
+import { RootStackScreenProps } from '../types'
 
-export const DiscoveryScreen = () => {
+interface IDiscoveryScreenProps {
+  navigation: RootStackScreenProps<'Discovery'>['navigation']
+  loginBottom: boolean
+}
+
+export const DiscoveryScreen: FC<IDiscoveryScreenProps> = ({
+  navigation,
+  loginBottom = false,
+}) => {
   const activeDealCategoryNames = useAppSelector(
     state => state.deals.activeDealCategoryNames
   )
@@ -12,6 +24,9 @@ export const DiscoveryScreen = () => {
   return isLoading || !data ? (
     <Text>Loading...</Text>
   ) : (
-    <DiscoveryView activeDealCategories={data} />
+    <>
+      <DiscoveryView activeDealCategories={data} />
+      {loginBottom && <LoginBottomNavigation navigation={navigation} />}
+    </>
   )
 }
