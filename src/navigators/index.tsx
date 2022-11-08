e53bda97ce24baaf6ac66034f6d188e3f4415b10
vzla0094 <vzla0094@gmail.com>
2022-11-08 10:51:44 -0700

    add icons to BottomNavigation

--- src/navigators/index.tsx
+++ src/navigators/index.tsx
@@ -1,6 +1,8 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { NavigationContainer } from '@react-navigation/native'
+import { FontAwesome } from '@expo/vector-icons'
+
 import { DiscoveryScreen } from '../screens/DiscoveryScreen'
 import { RootStackParamList } from '../types'
 import { LoginOrRegisterScreen } from '../screens/LoginOrRegisterScreen'
@@ -19,10 +21,42 @@ export default function RootNavigator() {
     <NavigationContainer>
       {authenticated ? (
         <Tab.Navigator screenOptions={{ headerShown: false }}>
-          <Tab.Screen name="Discovery" component={DiscoveryScreen} />
-          <Tab.Screen name="Search" component={SearchScreen} />
-          <Tab.Screen name="Profile" component={ProfileScreen} />
-          <Tab.Screen name="Settings" component={SettingsScreen} />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="home" color={color} size={size} />
+              ),
+            }}
+            name="Discovery"
+            component={DiscoveryScreen}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="search" color={color} size={size} />
+              ),
+            }}
+            name="Search"
+            component={SearchScreen}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="user" color={color} size={size} />
+              ),
+            }}
+            name="Profile"
+            component={ProfileScreen}
+          />
+          <Tab.Screen
+            options={{
+              tabBarIcon: ({ color, size }) => (
+                <FontAwesome name="gear" color={color} size={size} />
+              ),
+            }}
+            name="Settings"
+            component={SettingsScreen}
+          />
         </Tab.Navigator>
       ) : (
         <Stack.Navigator screenOptions={{ headerShown: false }}>
