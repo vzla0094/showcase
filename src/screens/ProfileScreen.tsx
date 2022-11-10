c62c484048b05c9bf2c6eb9a583a3a31f24a0ec3
vzla0094 <vzla0094@gmail.com>
2022-11-09 17:04:38 -0700

    add ProfileScreen details

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,9 +1,27 @@
-import { Center, Container, Heading } from 'native-base'
+import { Button, Center, Container, Divider, Heading, Input } from 'native-base'
 
 export const ProfileScreen = () => (
   <Center flex={1}>
-    <Container centerContent safeArea flex={1} w="100%">
-      <Heading>Profile screen</Heading>
+    <Container safeArea flex={1} w="100%">
+      <Heading>User details</Heading>
+      <Input size="lg" placeholder="Display name" />
+      <Input size="lg" placeholder="Birthday" />
+      <Input size="lg" placeholder="Phone number" />
+      <Button>Save user details</Button>
+      <Divider />
+      <Heading>Company details</Heading>
+      <Input size="lg" placeholder="Company name" />
+      <Heading size="md">Address</Heading>
+      <Input size="lg" placeholder="Street" />
+      <Input size="lg" placeholder="City" />
+      <Input size="lg" placeholder="State / Province" />
+      <Input size="lg" placeholder="Country" />
+      <Input size="lg" placeholder="Zip code" />
+      <Heading size="md">Contact information</Heading>
+      <Input size="lg" placeholder="Telephone number" />
+      <Input size="lg" placeholder="Cellphone number" />
+      <Input size="lg" placeholder="Email" />
+      <Button>Save company details</Button>
     </Container>
   </Center>
 )
