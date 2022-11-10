660ca0d8f07d6e8bdfba29b363efa21cf62babaf
vzla0094 <vzla0094@gmail.com>
2022-11-09 18:02:12 -0700

    initial Formik setup for user details

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,27 +1,94 @@
-import { Button, Center, Container, Divider, Heading, Input } from 'native-base'
+import {
+  Button,
+  Container,
+  FormControl,
+  Heading,
+  Input,
+  VStack,
+} from 'native-base'
+import { Formik } from 'formik'
 
-export const ProfileScreen = () => (
-  <Center flex={1}>
-    <Container safeArea flex={1} w="100%">
-      <Heading>User details</Heading>
-      <Input size="lg" placeholder="Display name" />
-      <Input size="lg" placeholder="Birthday" />
-      <Input size="lg" placeholder="Phone number" />
-      <Button>Save user details</Button>
-      <Divider />
-      <Heading>Company details</Heading>
-      <Input size="lg" placeholder="Company name" />
-      <Heading size="md">Address</Heading>
-      <Input size="lg" placeholder="Street" />
-      <Input size="lg" placeholder="City" />
-      <Input size="lg" placeholder="State / Province" />
-      <Input size="lg" placeholder="Country" />
-      <Input size="lg" placeholder="Zip code" />
-      <Heading size="md">Contact information</Heading>
-      <Input size="lg" placeholder="Telephone number" />
-      <Input size="lg" placeholder="Cellphone number" />
-      <Input size="lg" placeholder="Email" />
-      <Button>Save company details</Button>
+export const ProfileScreen = () => {
+  return (
+    <Container centerContent safeArea flex={1} alignSelf="center" width="100%">
+      <Formik
+        initialValues={{
+          username: '',
+          birthDay: '',
+          birthMonth: '',
+          birthYear: '',
+          phoneNumber: '',
+        }}
+        onSubmit={data => console.log('Submitting data: ', data)}
+      >
+        {({ handleBlur, handleChange, handleSubmit, values, errors }) => (
+          <VStack width="100%" space={2}>
+            <Heading>User details</Heading>
+            <FormControl isRequired isInvalid={'username' in errors}>
+              <Input
+                onBlur={handleBlur('username')}
+                onChangeText={handleChange('username')}
+                value={values.username}
+                placeholder="Name"
+                size="lg"
+              />
+              <FormControl.ErrorMessage>
+                {errors.username}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={'birthDay' in errors}>
+              <Input
+                onBlur={handleBlur('birthDay')}
+                onChangeText={handleChange('birthDay')}
+                value={values.birthDay}
+                placeholder="Day of birth"
+                size="lg"
+              />
+              <FormControl.ErrorMessage>
+                {errors.birthDay}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={'birthMonth' in errors}>
+              <Input
+                onBlur={handleBlur('birthMonth')}
+                onChangeText={handleChange('birthMonth')}
+                value={values.birthMonth}
+                placeholder="Month of birth"
+                size="lg"
+              />
+              <FormControl.ErrorMessage>
+                {errors.birthMonth}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={'birthYear' in errors}>
+              <Input
+                onBlur={handleBlur('birthYear')}
+                onChangeText={handleChange('birthYear')}
+                value={values.birthYear}
+                placeholder="Year of birth"
+                size="lg"
+              />
+              <FormControl.ErrorMessage>
+                {errors.birthYear}
+              </FormControl.ErrorMessage>
+            </FormControl>
+            <FormControl isRequired isInvalid={'phoneNumber' in errors}>
+              <Input
+                onBlur={handleBlur('phoneNumber')}
+                onChangeText={handleChange('phoneNumber')}
+                value={values.phoneNumber}
+                placeholder="Phone number"
+                size="lg"
+              />
+              <FormControl.ErrorMessage>
+                {errors.phoneNumber}
+              </FormControl.ErrorMessage>
+            </FormControl>
+
+            <Button onPress={() => handleSubmit()}>Save</Button>
+          </VStack>
+        )}
+      </Formik>
     </Container>
-  </Center>
-)
+  )
+}
