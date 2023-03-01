16cfa8458c7c5eeeac5150056e2b3fef609be162
Eliezer N. Valenzuela <vzla0094@gmail.com>
2023-03-01 19:55:47 +0000

    elie/94-ST-button

--- App.tsx
+++ App.tsx
@@ -1,7 +1,11 @@
 import 'react-native-gesture-handler'
 import { Provider as ReduxProvider } from 'react-redux'
 import { NativeBaseProvider } from 'native-base'
-import { Raleway_400Regular, useFonts } from '@expo-google-fonts/raleway'
+import {
+  Raleway_400Regular,
+  Raleway_600SemiBold,
+  useFonts,
+} from '@expo-google-fonts/raleway'
 
 import RootNavigator from './src/navigators'
 import { store } from './src/redux/store'
@@ -10,6 +14,7 @@ import { theme } from './src/theme'
 export default function App() {
   const [fontsLoaded] = useFonts({
     Raleway_400Regular,
+    Raleway_600SemiBold,
   })
 
   if (!fontsLoaded) {
